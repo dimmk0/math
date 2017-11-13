4e8e87671c551e8ef5dacbd265e7acebe84768b2
Dmytro Kaliazin <dkaliaz@cisco.com>
2017-11-13 13:12:10 +0200

    Add  calculate_determinant()
      calculate_determinant() actually does all work for matrix determinant
      calculation.

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -5,15 +5,19 @@
 """
 
 import numpy.random as nm
+from itertools import permutations
+from math import factorial
 
 
 class Matrix(object):
     """Class matrix container"""
-    def __init__(self, dimension=5):
+    def __init__(self, dimension=4):
+        self.dimension = dimension
         self.matrix = nm.random_integers(-5, 5, (dimension, dimension))
+        self.determinant = None
 
     def show_matrix(self):
-        """Just shows what we have"""
+        """Just shows input matrix that we have"""
         print str(self.matrix)
 
     def calculate_inversion(self, permutation):
@@ -23,9 +27,21 @@ class Matrix(object):
             inversion += len(filter(lambda x: x < permutation[index], permutation[index+1:]))
         return inversion
 
+    def calculate_determinant(self):
+        """Main calculations located there"""
+        permutation = permutations(range(self.dimension))
+        e = 0
+        for iteration in range(factorial(self.dimension)):
+            term = 1
+            permut = permutation.next()
+            for i in range(self.dimension):
+                term *= self.matrix[i][permut[i]]
+            e += (-1)**self.calculate_inversion(permut)*term
+        self.determinant = e
+        print e
+
+
 if __name__ == "__main__":
     x = Matrix()
     x.show_matrix()
-    perm = nm.random_integers(0, 5, 5)
-    print perm
-    print x.calculate_inversion(perm)
+    x.calculate_determinant()
